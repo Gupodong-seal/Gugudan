a8806500eb323f27609241a815e8a1f184d20bba
Gupodong-seal <Gupodong-seal@DESKTOP-UMLEOPG>
2022-01-24 18:22:00 +0900

    while문 for문 배우기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 public class Gugudan { 
 	public static void main(String[] args) {
+		 //6단
+//		int i = 1;
+//		while(i < 10) {
+//			System.out.println(6 * i);
+//			i = i + 1;
+//		 }
+		//7단
+		
+		for(int j = 1; j < 10; j++) {
+			System.out.println(7 * j);
+		}
+		
 		// 2단
 //		
 //		System.out.println("2단");
